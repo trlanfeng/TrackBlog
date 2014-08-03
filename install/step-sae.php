28f2fe27526deb39e1225befabde593f4a90a83d
trlanfeng <trlanfeng@foxmail.com>
2014-08-03 14:17:26 +0800

    更新SAE安装程序

--- install/step-sae.php
+++ install/step-sae.php
@@ -1,38 +1,41 @@
 <?php
-    if (!isset($_SERVER["HTTP_REFERER"])) {
-        header("Location: index.php");
-    }
-?>
-<?php
-    include "../config.php";
-    include SYS_ROOT . INC . 'Model/Base.php';
-    include SYS_ROOT . INC . 'Db/Mysql.php';
-    if (!empty($_POST['db_name'])) {
-        $db_name = "|".$_POST['db_host']."|".$_POST['db_username']."|".$_POST['db_password']."|".$_POST['db_name'];
-    } else {
-        $db_name = DB_NAME;
-    }
-    $tb = !empty($_POST['tb']) ? $_POST['tb'] : TB;
-    $nowdb = new Dbclass(SYS_ROOT . $db_name);
-    $creatTable = get_sql_mysql($tb);
-    $queryarray = explode(';', $creatTable);
-    //建立表数据
-    foreach ($queryarray as $k => $v) {
-        $nowdb->query($v) or die('creat table error' . $v);
-    }
-    //添加初始化数据
-    $nowdb->query('INSERT INTO ' . $tb . 'admin(name,passwd,auth,status)VALUES("admin","admin","admin",1)');
-    $nowdb->query('INSERT INTO ' . $tb . 'link (name,urls,content,status,orders) VALUES("TrackCMS","http://fengyu.name/?cat=zuopin&id=275","TrackCMS官方网站","1","0")');
-    $nowdb->query('INSERT INTO ' . $tb . 'category (name,fid,intro,orders,status,staticurl) VALUES("日记","0","日记本","0","1","?cat=1")');
-    $nowdb->query('INSERT INTO ' . $tb . 'cms (name,content,cat,times,orders,status,allowcmt,staticurl) VALUES("你的第一个小脚印出现在这里哦","TrackCMS已经正常运行了，记录你的梦想吧！觉得像博客？TrackCMS官方网站的CMSer模板给你打造一个门户网站！快去看看吧！http://fengyu.name/?cat=zuopin&id=275。【请您先到后台设置文章URL，然后生成URL】","0","1284920417","0","1","1","?id=1")');
 
-    $configs = file_get_contents('../config.php');
-    $_POST['tb'] && $configs = str_replace('define(\'TB\',	\'' . TB . '\');', 'define(\'TB\',	\'' . $_POST['tb'] . '\');', $configs);
-    $configs = str_replace('define(\'DB\',	\'' . DB . '\');', 'define(\'DB\',	\'Mysql\');', $configs);
-    $_POST['db_name'] && $configs = str_replace('define(\'DB_NAME\',	\'' . DB_NAME . '\');', 'define(\'DB_NAME\',	\'' . $_POST['db_name'] . '\');', $configs);
-    file_put_contents('../config.php', $configs);
-    file_put_contents('../data/install.lock', '');
-    header("Location: step-finish.php");
+include "../config.php";
+include SYS_ROOT . INC . 'Model/Base.php';
+include SYS_ROOT . INC . 'Db/Mysql.php';
+
+if (!empty($_POST['db_name'])) {
+    $db_name = "|".$_POST['db_host']."|".$_POST['db_username']."|".$_POST['db_password']."|".$_POST['db_name'];
+} else {
+    $db_name = DB_NAME;
+}
+$tb = !empty($_POST['tb']) ? $_POST['tb'] : TB;
+$nowdb = new Dbclass(SYS_ROOT . $db_name);
+
+$tmp = $nowdb->query("SELECT count(*) TABLES, table_schema FROM information_schema.TABLES  where table_schema = '" . SAE_MYSQL_DB . "' GROUP BY table_schema");
+if ($tmp['TABLES']) {
+    die('已经安装过，如需重新安装，请删除数据库中所有表');
+}
+
+$creatTable = get_sql_mysql($tb);
+$queryarray = explode(';', $creatTable);
+//建立表数据
+foreach ($queryarray as $k => $v) {
+    $nowdb->query($v) or die('creat table error' . $v);
+}
+//添加初始化数据
+$nowdb->query('INSERT INTO ' . $tb . 'admin(name,passwd,auth,status)VALUES("admin","admin","admin",1)');
+$nowdb->query('INSERT INTO ' . $tb . 'link (name,urls,content,status,orders) VALUES("TrackCMS","http://fengyu.name/?cat=zuopin&id=275","TrackCMS官方网站","1","0")');
+$nowdb->query('INSERT INTO ' . $tb . 'category (name,fid,intro,orders,status,staticurl) VALUES("日记","0","日记本","0","1","?cat=1")');
+$nowdb->query('INSERT INTO ' . $tb . 'cms (name,content,cat,times,orders,status,allowcmt,staticurl) VALUES("你的第一个小脚印出现在这里哦","TrackCMS已经正常运行了，记录你的梦想吧！觉得像博客？TrackCMS官方网站的CMSer模板给你打造一个门户网站！快去看看吧！http://fengyu.name/?cat=zuopin&id=275。【请您先到后台设置文章URL，然后生成URL】","0","1284920417","0","1","1","?id=1")');
+
+$configs = file_get_contents('../config.php');
+$_POST['tb'] && $configs = str_replace('define(\'TB\',	\'' . TB . '\');', 'define(\'TB\',	\'' . $_POST['tb'] . '\');', $configs);
+$configs = str_replace('define(\'DB\',	\'' . DB . '\');', 'define(\'DB\',	\'Mysql\');', $configs);
+$_POST['db_name'] && $configs = str_replace('define(\'DB_NAME\',	\'' . DB_NAME . '\');', 'define(\'DB_NAME\',	\'' . $_POST['db_name'] . '\');', $configs);
+file_put_contents('../config.php', $configs);
+file_put_contents('../data/install.lock', '');
+header("Location: step-finish.php");
 
 function get_sql_mysql($tb) {
     $creatTable = 
