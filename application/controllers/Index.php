9a6bcefa98cb755b86e37dc2a3761d3a13ee12dc
孤月蓝风 <trlanfeng@foxmail.com>
2015-12-13 02:08:48 +0800

    首页信息修改

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -23,19 +23,18 @@ class Index extends CI_Controller
         $filter = array();
         $catList = $this->category_model->getList($filter, 0, 0, 'orders ASC');
         $data['catlist'] = $catList;
-        $articleList= $this->article_model->getList($filter,10,($page - 1) * 10,"id DESC");
+        $articleList= $this->article_model->getList($filter,10,0,"id DESC");
         foreach ($articleList as $article) {
             $article['datetime'] = date('Y-m-d' ,$article['times']);
             $article['catname'] = $this->getCatnameById($article['cat']);
-            $article['tagarray'] = array();
-            $tagarray = explode(',', $article['tags']);
-            foreach ($tagarray as $tag) {
-                $tagOne['name'] = $tag;
-                $tagOne['url'] = '/tag/'.$tag;
-                $article['tagarray'][] = $tagOne;
-            }
+            $article['tagarray'] = explode(',', $article['tags']);;
             $data['articleList'][] = $article;
         }
+        $filter = array(
+            "cat"=>13
+        );
+        $works = $this->article_model->getList($filter,4,0,"id DESC");
+        $data['works'] = $works;
         $data['contentType'] = "index";
         $this->load->view('templates/'.TEMPLATES.'/header',$data);
         $this->load->view('templates/'.TEMPLATES.'/index',$data);
